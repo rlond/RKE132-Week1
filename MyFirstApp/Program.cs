5054fb54e833afcc786aae767603862c7e01fb6e
Rasmus L <rasmuslond1@gmail.com>
2023-01-25 19:34:19 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// This is a comment to my code
+
+// rakendus küsib kasutajal sisestada tema nime
+// rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //output
+// string - sõne
+string userName = Console.ReadLine(); //input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //output
+// string interpolation
+
+Console.WriteLine($"Hello, {userName} !"); //output
+
+
